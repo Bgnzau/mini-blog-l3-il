e7d056b660dccb2a722e70cffb2ceeb916f3f7b2
Dr-Lab1 <jbkukwabantu@gmail.com>
2026-06-03 06:01:50 +0100

    ...

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;    
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
